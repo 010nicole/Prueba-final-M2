8e12c02dfbab99e2b630ee1b3e367466375cce35
010nicole <nicol.maturanam@gmail.com>
2024-05-17 18:28:11 -0400

    modicando grillas de bootstrap

---
+++ README.md
@@ -0,0 +1,4 @@
+# PRUEBA FINAL M2 NICOLE MATURANA
+
+
+https://github.com/010nicole/Prueba-final-M2.git
---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+// genera tooltip en botones
+var myBttns = $(".connectiontooltip");
+console.log("--> ", myBttns);
+new bootstrap.Tooltip(myBttns[0]);
+new bootstrap.Tooltip(myBttns[1]);
+new bootstrap.Tooltip(myBttns[2]);
+new bootstrap.Tooltip(myBttns[3]);
