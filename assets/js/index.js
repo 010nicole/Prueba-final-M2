1e9fe5d4609bda225c86bc88058462881bc931cf
010nicole <nicol.maturanam@gmail.com>
2024-05-18 02:12:25 -0400

    agregando tooltip, click y doble click  al documento

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,39 @@
-// genera tooltip en botones
+// genera tooltip en botones del footer
 var myBttns = $(".connectiontooltip");
 console.log("--> ", myBttns);
 new bootstrap.Tooltip(myBttns[0]);
 new bootstrap.Tooltip(myBttns[1]);
 new bootstrap.Tooltip(myBttns[2]);
 new bootstrap.Tooltip(myBttns[3]);
+new bootstrap.Tooltip(myBttns[4]);
+new bootstrap.Tooltip(myBttns[5]);
+new bootstrap.Tooltip(myBttns[6]);
+// desaparece contenido en tarjetas al hacer click en img
+$(document).ready(function(){
+    $(".card-img-top").click(function(){
+          $(".card-text").toggle();
+    });
+    });
+
+// cambia de color titulos al hacer doble click
+$(document).ready(function () {
+    $(".h3-jq1").on("dblclick", function () {
+          $(this).css({
+                "color": "red",
+          });
+    });
+});
+$(document).ready(function () {
+    $(".h3-jq2").on("dblclick", function () {
+          $(this).css({
+                "color": "red",
+          });
+    });
+});
+
+// genera alerta al hacer click
+$(document).ready(function(){
+    $(".btn").click(function(){
+    alert("El formulario ha sido enviado exitosamente");
+    });
+    });
