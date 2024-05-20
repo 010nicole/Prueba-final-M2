04e0d95f4101918af9ce31102cc582fcc65d565f
010nicole <nicol.maturanam@gmail.com>
2024-05-20 17:20:51 -0400

    modificando estilos finales

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # PRUEBA FINAL MÓDULO 2 -  NICOLE MATURANA
 Este es un código sobre viajes dentro de Chile para un bootcamp, en el que se utilizó un navbar, carousel, tarjetas y formulario de Bootstrap, además de iconos.
 Se utilizó tooltip y href en los iconos del footer.
-Se utilizó un click que cambia de color 
+Se utilizó un click que cambia de color el background de la sección quienes somos.
+Se utilizó un scroll en el navbar.
+Se utilizó un click en el botón enviar del formulario.
+
 
 https://github.com/010nicole/Prueba-final-M2.git
--- assets/js/index.js
+++ assets/js/index.js
@@ -8,32 +8,56 @@ new bootstrap.Tooltip(myBttns[3]);
 new bootstrap.Tooltip(myBttns[4]);
 new bootstrap.Tooltip(myBttns[5]);
 new bootstrap.Tooltip(myBttns[6]);
+
 // desaparece contenido en tarjetas al hacer click en img
-$(document).ready(function(){
-    $(".card-img-top").click(function(){
-          $(".card-text").toggle();
-    });
-    });
+$(document).ready(function () {
+      $(".card-img-top").click(function () {
+            $(".card-text").toggle();
+      });
+});
 
 // cambia de color titulos al hacer doble click
 $(document).ready(function () {
-    $(".h3-jq1").on("dblclick", function () {
-          $(this).css({
-                "color": "red",
-          });
-    });
-});
-$(document).ready(function () {
-    $(".h3-jq2").on("dblclick", function () {
-          $(this).css({
-                "color": "red",
-          });
-    });
+      $(".card-body").on("dblclick", function () {
+            $(this).css({
+                  "background-color": "red",
+            });
+      });
 });
 
 // genera alerta al hacer click
-$(document).ready(function(){
-    $(".btn").click(function(){
-    alert("El formulario ha sido enviado exitosamente");
-    });
-    });
+$(document).ready(function () {
+      $(".btn").click(function () {
+            alert("El formulario ha sido enviado exitosamente");
+      });
+});
+
+// cambiar color de navbar
+$(document).scroll(function (e) {
+      const ubicacionScroll = $("html").scrollTop();
+      console.log(" ubicacion scroll es:", ubicacionScroll);
+      if (ubicacionScroll > 800) {
+            $("nav").removeClass("navTransparent").addClass("navWhite")
+      } else {
+            $("nav").removeClass("navWhite").addClass("navTransparent")
+      }
+})
+
+// Intento de cambiar el color del navbar tanto para ventana de pc como de mobile
+// $(document).scroll(function (e) {
+//       const ubicacionScroll = $("html").scrollTop();
+//       if ((document).width() > "380") {
+//             if (ubicacionScroll >= 800) {
+//                   $("nav").removeClass("navTransparent").addClass("navWhite")
+//             } else {
+//                   $("nav").removeClass("navWhite").addClass("navTransparent")
+//             }
+//       } else {
+//             if (ubicacionScroll >= 250) {
+//                   $("nav").removeClass("navTransparent").addClass("navWhite")
+//             } else {
+//                   $("nav").removeClass("navWhite").addClass("navTransparent")
+//             }
+//       }
+
+// })
